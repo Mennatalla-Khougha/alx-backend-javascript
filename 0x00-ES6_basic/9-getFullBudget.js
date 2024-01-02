d462eebd12521eb86024df6edd4e73265434c064
Mennatalla-Khougha <menmen.khougha@gmail.com>
2024-01-02 19:22:04 +0200

    fix task 9

--- 0x00-ES6_basic/9-getFullBudget.js
+++ 0x00-ES6_basic/9-getFullBudget.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 /* eslint-disable import/extensions */
 
-import getBudgetObject from './7-getBudgetObject.js';
+import getBudgetObject from './7-getBudgetObject';
 
 export default function getFullBudgetObject(income, gdp, capita) {
   const budget = getBudgetObject(income, gdp, capita);
