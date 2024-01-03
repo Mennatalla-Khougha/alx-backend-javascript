628ebc4aa0f96870c378625c653dfb20528d0aa9
Mennatalla-Khougha <menmen.khougha@gmail.com>
2024-01-03 09:48:38 +0200

    task 2

--- 0x01-ES6_promise/2-then.js
+++ 0x01-ES6_promise/2-then.js
@@ -1,1 +1,14 @@
-#!/usr/bin/node
+function handleResponseFromAPI(promise) {
+  const isSuccess = {
+    status: 200,
+    body: 'success',
+  };
+
+  return promise
+    .then(() => {
+      console.log('Got a response from the API');
+      return (isSuccess);
+    }).catch(() => (new Error()));
+}
+
+module.exports = handleResponseFromAPI;
