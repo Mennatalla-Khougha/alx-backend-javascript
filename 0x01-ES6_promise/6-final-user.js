21e4de76458f566ae401866cf0b1e329b09f038d
Mennatalla-Khougha <menmen.khougha@gmail.com>
2024-01-03 10:55:59 +0200

    task 6

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -1,1 +1,22 @@
-#!/usr/bin/node
+// /* eslint-disable import/extensions */
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+async function handleProfileSignup(firstName, lastName, fileName) {
+  const [user, photo] = await Promise.allSettled(
+    [signUpUser(fileName, lastName), uploadPhoto(fileName)],
+  );
+
+  return [
+    {
+      status: user.status,
+      value: user.value || user.reason,
+    },
+    {
+      status: photo.status,
+      value: photo.value || photo.reason,
+    },
+  ];
+}
+
+export default handleProfileSignup;
