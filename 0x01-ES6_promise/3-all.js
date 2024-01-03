4f18c38ee4fc0079166cf9795b32fec3ccccc5a9
Mennatalla-Khougha <menmen.khougha@gmail.com>
2024-01-03 10:20:46 +0200

    task 3

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,1 +1,11 @@
-#!/usr/bin/node
+import { createUser, uploadPhoto } from './utils';
+
+async function handleProfileSignup() {
+  const [photo, user] = await Promise.all([uploadPhoto(), createUser()]);
+  const { body } = photo;
+  const { firstName, lastName } = user;
+
+  console.log(`${body} ${firstName} ${lastName}`);
+}
+
+module.exports = handleProfileSignup;
