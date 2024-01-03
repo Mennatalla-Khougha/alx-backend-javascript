437ef8e6c6c165a503154249e9e52969f189ab28
Mennatalla-Khougha <menmen.khougha@gmail.com>
2024-01-03 14:44:08 +0200

    task 9

--- 0x01-ES6_promise/9-try.js
+++ 0x01-ES6_promise/9-try.js
@@ -1,1 +1,13 @@
-#!/usr/bin/node
+export default function guardrail(mathFunction) {
+  const queue = [];
+
+  try {
+    queue.push(mathFunction());
+  } catch (error) {
+    queue.push(`Error: ${error.message}`);
+  }
+
+  queue.push('Guardrail was processed');
+
+  return queue;
+}
