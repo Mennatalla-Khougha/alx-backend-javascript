d6e51e2fa97196ab28c998c327358f3d1375760c
Mennatalla-Khougha <menmen.khougha@gmail.com>
2024-01-03 10:40:02 +0200

    task 5

--- 0x01-ES6_promise/5-photo-reject.js
+++ 0x01-ES6_promise/5-photo-reject.js
@@ -1,5 +1,4 @@
 export default function uploadPhoto(filename) {
-  return new Promise((reject) => {
-    reject(new Error(`${filename} cannot be processed`));
+  return Promise.reject(Error(`${filename} cannot be processed`));
   });
 }
