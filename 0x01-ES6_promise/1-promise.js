47317e3cf22fb6e09c91f0fa899eaa61ad181c60
Mennatalla-Khougha <menmen.khougha@gmail.com>
2024-01-03 09:32:53 +0200

    task 1

--- 0x01-ES6_promise/1-promise.js
+++ 0x01-ES6_promise/1-promise.js
@@ -1,1 +1,16 @@
-#!/usr/bin/node
+function getFullResponseFromAPI(success) {
+  const isSuccess = {
+    status: 200,
+    body: 'Success',
+  };
+
+  return new Promise((resolve, reject) => {
+    if (success) {
+      resolve(isSuccess);
+    } else {
+      reject(new Error('The fake API is not working currently'));
+    }
+  });
+}
+
+module.exports = getFullResponseFromAPI;
