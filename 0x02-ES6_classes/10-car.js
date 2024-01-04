28b26c9be9c583585918df5a3f1ed5248e5f9e29
Mennatalla-Khougha <menmen.khougha@gmail.com>
2024-01-04 21:57:52 +0200

    task 10

--- 0x02-ES6_classes/10-car.js
+++ 0x02-ES6_classes/10-car.js
@@ -1,1 +1,13 @@
-#!/usr/bin/node
+export default class Car {
+  constructor(brand, motor, color) {
+    this._brand = brand;
+    this._motor = motor;
+    this._color = color;
+  }
+
+  cloneCar() {
+    const car = Object.create(this);
+    car[Symbol.for('car')] = true;
+    return car;
+  }
+}
