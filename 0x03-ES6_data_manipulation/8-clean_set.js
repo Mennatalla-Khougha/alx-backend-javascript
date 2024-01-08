226ec12c7ebeabcee386505cc75b22e2bc8652d7
Mennatalla-Khougha <menmen.khougha@gmail.com>
2024-01-08 23:08:39 +0200

    task 8

--- 0x03-ES6_data_manipulation/8-clean_set.js
+++ 0x03-ES6_data_manipulation/8-clean_set.js
@@ -1,8 +1,7 @@
 export default function cleanSet(set, startString) {
-  if (startString.length === 0 || startString === undefined) {
+  if (startString === undefined || startString.length === 0) {
     return '';
   }
-
   return [...set]
     .filter((ele) => (ele !== undefined ? ele.startsWith(startString) : ''))
     .map((ele) => (ele !== undefined ? ele.slice(startString.length) : ''))
