d2762c24ec3642aac103a5da5ddee404467f57b3
Mennatalla-Khougha <menmen.khougha@gmail.com>
2024-02-26 22:56:52 +0200

    task 3

--- 0x05-Node_JS_basic/3-read_file_async.js
+++ 0x05-Node_JS_basic/3-read_file_async.js
@@ -1,14 +1,14 @@
 const fs = require('fs').promises;
 
-function countStudents (file) {
+function countStudents(file) {
   return new Promise((resolve, reject) => {
     fs.readFile(file, 'utf-8')
-      .then(data => {
-        const lines = data.toString().split('\n');
+      .then((data) => {
+        const lines = data.toString().split('\n').filter((line) => line.trim() !== '');
         let students = 0;
         const subjects = {};
 
-        for (let i = 1; i < lines.length; i++) {
+        for (let i = 1; i < lines.length; i += 1) {
           const line = lines[i];
           const fields = line.split(',');
           const field = fields[3];
@@ -17,17 +17,17 @@ function countStudents (file) {
           if (!subjects[field]) {
             subjects[field] = { count: 0, names: [] };
           }
-          subjects[field].count++;
+          subjects[field].count += 1;
           subjects[field].names.push(firstName);
-          students++;
+          students += 1;
         }
         console.log(`Number of students: ${students}`);
-        Object.keys(subjects).forEach(field => {
+        Object.keys(subjects).forEach((field) => {
           console.log(`Number of students in ${field}: ${subjects[field].count}. List: ${subjects[field].names.join(', ')}`);
         });
         resolve();
       })
-      .catch(error => {
+      .catch(() => {
         reject(new Error('Cannot load the database'));
       });
   });
--- 0x05-Node_JS_basic/main.js
+++ 0x05-Node_JS_basic/main.js
@@ -1,3 +1,8 @@
-const countStudents = require('./2-read_file');
-
-countStudents("database.csv");
+countStudents("database.csv")
+    .then(() => {
+        console.log("Done!");
+    })
+        .catch((error) => {
+        console.log(error);
+    });
+console.log("After!");
