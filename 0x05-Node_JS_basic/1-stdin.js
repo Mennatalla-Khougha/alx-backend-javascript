19289058a3ea46c62e7ae0f2181a5b44f9d417eb
Mennatalla-Khougha <menmen.khougha@gmail.com>
2024-02-26 17:25:45 +0200

    task 1

--- 0x05-Node_JS_basic/1-stdin.js
+++ 0x05-Node_JS_basic/1-stdin.js
@@ -2,17 +2,17 @@ console.log('Welcome to Holberton School, what is your name?');
 
 if (process.stdin.isTTY) {
   process.stdin.on('data', (data) => {
-    const input = data.toString();
-    console.log(`Your name is: ${input}`);
+    const input = data.toString().trim();
+    process.stdout.write(`Your name is: ${input}`);
     process.exit(0);
   });
 } else {
   process.stdin.on('data', (data) => {
-    const input = data.toString();
-    console.log(`Your name is: ${input}`);
+    const input = data.toString().trim();
+    process.stdout.write(`Your name is: ${input}`);
     process.exit();
   });
   process.on('exit', () => {
-    console.log('This important software is now closing');
+    process.stdout.write('This important software is now closing');
   });
 }
