b898ac3610a007d647ce4345146fe0114f6933bd
Mennatalla-Khougha <menmen.khougha@gmail.com>
2024-02-27 02:51:18 +0200

    task 8

--- 0x05-Node_JS_basic/full_server/utils.js
+++ 0x05-Node_JS_basic/full_server/utils.js
@@ -1,28 +1,26 @@
-import fs from 'fs';
-import util from 'util';
+const { readFile } = require('fs');
 
-const rl = util.promisify(fs.readFile);
-
-async function readDatabase(file) {
-  try {
-    const data = await rl(file, 'utf-8');
-    const lines = data.toString().split('\n').filter((line) => line.trim() !== '');
-    const students = {};
-
-    for (let i = 1; i < lines.length; i += 1) {
-      const line = lines[i].split(',');
-      const name = line[0];
-      const subject = line[3];
-
-      if (!students[subject]) {
-        students[subject] = [];
+function readDatabase(file) {
+  const students = {};
+  return new Promise((resolve, reject) => {
+    readFile(file, (err, data) => {
+      if (err) {
+        reject(err);
+      } else {
+        const lines = data.toString().split('\n').filter((line) => line.trim() !== '');
+        for (let i = 1; i < lines.length; i += 1) {
+          const line = lines[i].split(',');
+          const name = line[0];
+          const subject = line[3];
+          if (!students[subject]) {
+            students[subject] = [];
+          }
+          students[subject].push(name);
+        }
+        resolve(students);
       }
-      students[subject].push(name);
-    }
-    return students;
-  } catch (err) {
-    return Promise.reject(err);
-  }
+    });
+  });
 }
 
 module.exports = readDatabase;
