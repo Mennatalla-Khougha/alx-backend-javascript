36d7cd98e58044b37a22ddc43cd8a12a37210b9e
Mennatalla-Khougha <menmen.khougha@gmail.com>
2024-02-29 01:31:40 +0200

    task 9

--- 0x06-unittests_in_js/9-api/api.test.js
+++ 0x06-unittests_in_js/9-api/api.test.js
@@ -1,1 +1,42 @@
-#!/usr/bin/node
+const request = require("request");
+const expect = require("chai").expect;
+
+describe("Index page", function() {
+    const data = {
+	url: "http://localhost:7865/",
+	method: "GET"
+    }
+    it("check correct status code", (done) => {
+	request(data, function(err, res, body) {
+	    expect(res.statusCode).to.equal(200);
+	    done();
+	});
+    });
+    it("check correct content", (done) => {
+	request(data, function(err, res, body) {
+	    expect(body).to.contain("Welcome to the payment system");
+	    done();
+	});
+    });
+});
+
+describe("Cart page", function () {
+	it("check correct status code for correct url", function (done) {
+		request.get("http://localhost:7865/cart/50", function (err, res, body) {
+			expect(res.statusCode).to.equal(200);
+			done();
+		});
+	});
+	it("check correct content for correct url", function (done) {
+		request.get("http://localhost:7865/cart/50", function (err, res, body) {
+			expect(body).to.contain("Payment methods for cart 50");
+			done();
+		});
+	});
+	it("check correct status code for incorrect url", function (done) {
+		request.get("http://localhost:7865/cart/err", function (err, res, body) {
+			expect(res.statusCode).to.equal(404);
+			done();
+		});
+	});
+});
