979b1583544f45eb61d2b115d7df9d14adaa35b3
Mennatalla-Khougha <menmen.khougha@gmail.com>
2024-02-28 20:16:21 +0200

    task 0

--- 0x06-unittests_in_js/3-payment.js
+++ 0x06-unittests_in_js/3-payment.js
@@ -1,1 +1,5 @@
-#!/usr/bin/node
+const Utils = require('./utils')
+
+function sendPaymentRequestToApi(totalAmount, totalShipping) {
+    
+}
--- 0x06-unittests_in_js/utils.js
+++ 0x06-unittests_in_js/utils.js
@@ -1,1 +1,21 @@
 #!/usr/bin/node
+
+const Utils = {
+    calculateNumber: function(type, a, b) {
+        if (type === 'SUM') {
+            return Math.round(a) + Math.round(b);
+          }
+          if (type === 'SUBTRACT') {
+            return Math.round(a) - Math.round(b);
+          }
+          if (type === 'DIVIDE') {
+            if (Math.round(b) === 0) {
+              return 'ERROR';
+            }
+            return Math.round(a) / Math.round(b);
+          }
+       // Add more operations as needed
+    }
+   };
+   
+   module.exports = Utils;
