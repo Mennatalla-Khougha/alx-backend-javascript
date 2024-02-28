9b5651364b0d4be0d3e1a97fa12a151e8eb1bb9e
Mennatalla-Khougha <menmen.khougha@gmail.com>
2024-02-29 01:11:49 +0200

    task 8

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -6,21 +6,21 @@ const chai = require('chai');
 chai.use(chaiHttp);
 
 describe('Index Page', () => {
-    it('should return the correct status code', (done) => {
-        chai.request(app)
-            .get('/')
-            .end((err, res) => {
-                expect(res).to.have.status(200);
-                done();
-            });
-    });
+  it('should return the correct status code', (done) => {
+    chai.request(app)
+      .get('/')
+      .end((err, res) => {
+        expect(res).to.have.status(200);
+        done();
+      });
+  });
 
-    it('should return the correct result', (done) => {
-        chai.request(app)
-            .get('/')
-            .end((err, res) => {
-                expect(res.text).to.equal('Welcome to the payment system');
-                done();
-            });
-    });
+  it('should return the correct result', (done) => {
+    chai.request(app)
+      .get('/')
+      .end((err, res) => {
+        expect(res.text).to.equal('Welcome to the payment system');
+        done();
+      });
+  });
 });
