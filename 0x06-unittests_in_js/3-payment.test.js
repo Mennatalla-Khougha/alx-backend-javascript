67bcd1d3ce3e490fd95bb2e4c47aca87936d418c
Mennatalla-Khougha <menmen.khougha@gmail.com>
2024-02-28 23:44:47 +0200

    task 3

--- 0x06-unittests_in_js/3-payment.test.js
+++ 0x06-unittests_in_js/3-payment.test.js
@@ -1,19 +1,13 @@
 const sinon = require('sinon');
-// const { expect } = require('chai');
+const { expect } = require('chai');
 const sendPaymentRequestToApi = require('./3-payment');
 const Utils = require('./utils');
 
-let expect;
-before(async () => {
-  const chaiModule = await import('chai');
-  expect = chaiModule.expect;
-});
-
 describe('sendPaymentRequestToApi', () => {
-  it('should use Utils.calculateNumber with type SUM', () => {
-    const calculateNumberSpy = sinon.spy(Utils, 'calculateNumber');
-    sendPaymentRequestToApi(100, 200);
-    expect(calculateNumberSpy.calledWith('SUM', 100, 200)).to.be.true;
-    calculateNumberSpy.restore();
-  });
+    it('should use Utils.calculateNumber with type SUM', () => {
+        const calculateNumberSpy = sinon.spy(Utils, 'calculateNumber');
+        sendPaymentRequestToApi(100, 200);
+        expect(calculateNumberSpy.calledWith('SUM', 100, 200)).to.be.true;
+        calculateNumberSpy.restore();
+    });
 });
