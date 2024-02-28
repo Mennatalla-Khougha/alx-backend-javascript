6e995cc7b4babac9d331cfdc38e2e529674b0d3c
Mennatalla-Khougha <menmen.khougha@gmail.com>
2024-02-29 01:23:53 +0200

    task 8

--- 0x06-unittests_in_js/8-api/api.test.js
+++ 0x06-unittests_in_js/8-api/api.test.js
@@ -1,26 +1,25 @@
 const request = require("request");
-const {describe, it} = require("mocha");
 const expect = require("chai").expect;
 
 describe("Index page", function() {
-    const options = {
+    const data = {
 	url: "http://localhost:7865/",
 	method: "GET"
     }
-    it("check correct status code", function(done) {
-	request(options, function(err, res, body) {
+    it("check correct status code", (done) => {
+	request(data, function(err, res, body) {
 	    expect(res.statusCode).to.equal(200);
 	    done();
 	});
     });
-    it("check correct content", function(done) {
-	request(options, function(err, res, body) {
+    it("check correct content", (done) => {
+	request(data, function(err, res, body) {
 	    expect(body).to.contain("Welcome to the payment system");
 	    done();
 	});
     });
-    it("check correct content length", function(done) {
-	request(options, function(err, res, body) {
+    it("check correct content length", (done) => {
+	request(data, function(err, res, body) {
 	    expect(res.headers['content-length']).to.equal('29');
 	    done();
 	});
