d8e6dcf5548671516082d67fe51ec51d4d01934c
Mennatalla-Khougha <menmen.khougha@gmail.com>
2024-02-28 23:58:53 +0200

    task 4

--- 0x06-unittests_in_js/4-payment.test.js
+++ 0x06-unittests_in_js/4-payment.test.js
@@ -1,1 +1,15 @@
-#!/usr/bin/node
+const sinon = require('sinon');
+const { expect } = require('chai');
+const sendPaymentRequestToApi = require('./3-payment');
+const Utils = require('./utils');
+
+describe('sendPaymentRequestToApi', () => {
+    it('should use Utils.calculateNumber with type SUM and return 10', () => {
+        const calculateNumberStub = sinon.stub(Utils, 'calculateNumber').returns(10);
+        const calculateNumberSpy = sinon.spy(console, 'log');
+        sendPaymentRequestToApi(100, 20);
+        expect(calculateNumberSpy.calledWith('SUM', 100, 20)).to.be.true;
+        calculateNumberStub.restore();
+        calculateNumberSpy.restore();
+    });
+});
