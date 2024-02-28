e2105d5c224e5df3864dc2eaf98cd17fa314135e
Mennatalla-Khougha <menmen.khougha@gmail.com>
2024-02-29 00:47:06 +0200

    task 6

--- 0x06-unittests_in_js/6-payment_token.test.js
+++ 0x06-unittests_in_js/6-payment_token.test.js
@@ -1,1 +1,14 @@
-#!/usr/bin/node
+const { expect } = require('chai');
+const getPaymentTokenFromAPI = require('./6-payment_token');
+
+describe('getPaymentTokenFromAPI', () => {
+  it('should return a resolved promise with an object', async () => {
+    const result = await getPaymentTokenFromAPI(true);
+    expect(result).to.deep.equal({ data: 'Successful response from the API' });
+  });
+
+  it('should not return anything when success is false', async () => {
+    const result = await getPaymentTokenFromAPI(false);
+    expect(result).to.be.undefined;
+  });
+});
