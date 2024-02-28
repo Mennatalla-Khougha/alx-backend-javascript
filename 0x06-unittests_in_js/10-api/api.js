de1e6f71662f9662c6d20c718c3e5f91ce309178
Mennatalla-Khougha <menmen.khougha@gmail.com>
2024-02-29 01:52:44 +0200

    task 10

--- 0x06-unittests_in_js/10-api/api.js
+++ 0x06-unittests_in_js/10-api/api.js
@@ -1,1 +1,37 @@
-#!/usr/bin/node
+const express = require('express');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('Welcome to the payment system');
+});
+
+app.get('/cart/:id([0-9]+)', (req, res) => {
+  res.send(`Payment methods for cart ${req.params.id}`);
+});
+
+app.get('/available_payments', (request, res) => {
+  res.set("Content-Type", "application/json");
+  const payMethods = {
+        payment_methods: {
+          credit_cards: true,
+          paypal: false
+}
+  }
+  res.send(payMethods);
+});
+
+app.post('/login', (req, res) => {
+  const userName = req.body.userName;
+  if (userName) {
+res.send(`Welcome ${userName}`);
+  } else {
+res.status(404).send();
+  }
+});
+
+app.listen(7865, () => {
+  console.log('API available on localhost port 7865');
+});
+
+module.exports = app;
--- 0x06-unittests_in_js/10-api/api.test.js
+++ 0x06-unittests_in_js/10-api/api.test.js
@@ -1,1 +1,116 @@
-#!/usr/bin/node
+const request = require("request");
+const expect = require("chai").expect;
+
+describe("Index page", function() {
+    const data = {
+	url: "http://localhost:7865/",
+	method: "GET"
+    }
+    it("check correct status code", (done) => {
+	request(data, function(err, res, body) {
+	    expect(res.statusCode).to.equal(200);
+	    done();
+	});
+    });
+    it("check correct content", (done) => {
+	request(data, function(err, res, body) {
+	    expect(body).to.contain("Welcome to the payment system");
+	    done();
+	});
+    });
+});
+
+describe("Cart page", function () {
+	it("check correct status code for correct url", function (done) {
+		request.get("http://localhost:7865/cart/50", function (err, res, body) {
+			expect(res.statusCode).to.equal(200);
+			done();
+		});
+	});
+	it("check correct content for correct url", function (done) {
+		request.get("http://localhost:7865/cart/50", function (err, res, body) {
+			expect(body).to.contain("Payment methods for cart 50");
+			done();
+		});
+	});
+	it("check correct status code for incorrect url", function (done) {
+		request.get("http://localhost:7865/cart/err", function (err, res, body) {
+			expect(res.statusCode).to.equal(404);
+			done();
+		});
+	});
+});
+
+describe("Available_payments page", () => {
+    it("check correct status for url", () => {
+	request.get("http://localhost:7865/available_payments", (err, res, body) => {
+	    if (err) {
+		expect(res.statusCode).to.not.equal(200);
+	    } else {
+		expect(res.statusCode).to.equal(200);
+	    }
+	});
+    });
+    it("check correct body content for url", () => {
+	const data = {json: true};
+	const payment = {
+	    payment_methods: {
+		credit_cards: true,
+		paypal: false
+	    }
+	}
+	request.get("http://localhost:7865/available_payments", data, (err, res, body) => {
+	    if (err) {
+		expect(res.statusCode).to.not.equal(200);
+	    } else {
+		expect(body).to.deep.equal(payment);
+	    }
+	});
+    });
+});
+
+describe("Login", function() {
+    it("check correct status code for a good request", (done) => {
+	const data = {
+	    url: "http://localhost:7865/login",
+	    json: true,
+	    body: {
+		userName: 'JOE'
+	    }
+	};
+	request.post(data, function(err, res, body) {
+	    expect(res.statusCode).to.equal(200);
+	    done();
+	});
+    });
+    it("check correct content for a good request", (done) => {
+	const data = {
+	    url: "http://localhost:7865/login",
+	    json: true,
+	    body: {
+		userName: 'JOE'
+	    }
+	};
+	request.post(data, function(err, res, body) {
+	    if (err) {
+		expect(res.statusCode).to.not.equal(200);
+	    } else {
+		expect(body).to.contain('Welcome JOE');
+	    }
+	    done();
+	});
+    });
+    it("check correct status code for a bad request", (done) => {
+	const data = {
+	    url: "http://localhost:7865/login",
+	    json: true,
+	    body: {
+		userName: 'JOE'
+	    }
+	};
+	request.post(data, function(err, res, body) {
+	    expect(res.statusCode).to.equal(404);
+	    done();
+	});
+  });
+});
